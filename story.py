db3402a45ebedf4edd60c8536254ac0259a5d87e
Harshal M. <harshal.mehta@competitivekids.org>
2025-07-10 14:35:39 -0400

    Create story.py

---
+++ story.py
@@ -0,0 +1,24 @@
+name = input("what is your name: ")
+nm =  "{name}"
+name2 = "Jarry"
+place = "Paris"
+place2 = "france"
+adjective = "gigantic"
+adjective2 = "numerous"
+noun = "Roses"
+noun2 = "bamboo"
+noun3 = "apples"
+noun4 = "tree"
+adjective3 = "enchanting"
+noun5 = "vegetables"
+noun6 = "tomatoes"
+noun7 = "potatoes"
+noun8 = "lettuce"
+noun9 = "candy"
+noun10 = "candies"
+adverb = "quickly"
+noun12 = "security camaras"
+adjective4 = "unique"
+
+
+print(f"{nm} had a {adjective} garden in {place}. The garden had {adjective2} {noun} and {noun2} plants and {noun3} hanging from a {noun4}. There was even an {adjective3} section of the garden for {noun5} like {noun6}, {noun7}, and {noun8}. {name2} said they should put in a piece of {noun9} to grow {noun10}. People from all over {place2} came to sample some of the special {noun9} plants. Because the {noun9} plants grew so {adverb} they were protected with {noun12}. One day a judge from a farm in {place} awarded {nm} for the most {adjective4} garden in {place2}")
