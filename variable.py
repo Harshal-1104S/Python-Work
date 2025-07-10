c2abca6d33d1318feff30b4de04697a32e8ebef0
Harshal M. <harshal.mehta@competitivekids.org>
2025-07-10 14:33:39 -0400

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,8 @@
+noun = "car"
+adverb = "gently"
+adjective = "loud"
+print("The {noun} was {adjective} when it {adverb} went to school")
+noun = "zebra"
+adverb = "agressively"
+adjective = "giant"
+print("The {noun} was {adjective} when it {adverb} went to school")
