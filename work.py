417f237404d79de5b1bbe2f427a9725006c0ebd6
Harshal M. <harshal.mehta@competitivekids.org>
2025-07-10 14:33:14 -0400

    Create work.py

---
+++ work.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you?")) #asks user for age
+if age == (18, 30):  #if age entered is between 18-30
+    print("You are eligible for the youth discount.") #then they are eligible
+else:
+    print("You are not eligible for the youth discount.") #if not they aren't eligible
